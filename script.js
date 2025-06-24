a142cbfe85d3bdf59ee173ae9ec9684d956b7045
biancakrambeck <bianca.krambeck.santos@escola.pr.gov.br>
2025-06-24 14:24:34 -0300

    .

--- script.js
+++ script.js
@@ -5,4 +5,6 @@ botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 function mostraPalavrasChave() {
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
+    
+    campoResultado.textContent = texto;
 }
